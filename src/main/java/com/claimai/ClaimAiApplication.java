44fdfce192e171443c8a51704799630bb8c93f95
apurupa810-dotcom <apurupa810@gmail.com>
2026-04-22 15:25:40 -0400

    Create main Spring Boot application class

---
+++ src/main/java/com/claimai/ClaimAiApplication.java
@@ -0,0 +1,10 @@
+package com.claimai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringbootApplication
+  public class ClaimAiApplication{
+    public static void main(String[] args){
+      springApplication.run(ClaimAiApplication.clas, args);
+    }
+  }
