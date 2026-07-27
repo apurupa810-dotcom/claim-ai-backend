5e0d68a85f3f754d87fa7b28090bb3d3cde74704
apurupa810-dotcom <apurupa810@gmail.com>
2026-07-27 22:56:35 +0530

    Update ClaimResponse.java

--- src/main/java/com/claimai/dto/ClaimResponse.java
+++ src/main/java/com/claimai/dto/ClaimResponse.java
@@ -1,25 +1,24 @@
 package com.claimai.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ClaimResponse {
-    
+
     private String claimId;
     private String fraudRisk;
     private String approvalConfidence;
     private String recommendation;
     private String message;
     private Double processedAmount;
-    
-    // Constructor for easy creation
-    public ClaimResponse(String claimId, String fraudRisk, String approvalConfidence, 
-                        String recommendation, String message, Double processedAmount) {
-        this.claimId = claimId;
-        this.fraudRisk = fraudRisk;
-        this.approvalConfidence = approvalConfidence;
-        this.recommendation = recommendation;
-        this.message = message;
-        this.processedAmount = processedAmount;
-    }
+    private boolean fallback;
+    private AgentResults agents;
 }
